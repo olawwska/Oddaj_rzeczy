fe21a394dcef28473622ba25a44c802ec13851ee
olawwska <olwojciech@gmail.com>
2019-05-20 12:56:20 +0200

    four steps section

--- src/FourStepsSection/FourStepsSection.jsx
+++ src/FourStepsSection/FourStepsSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './FourStepsSection.module.css';
 import Decoration from '../Decoration/Decoration';
+import Button from '../Button/Button';
 
 export default class FourStepsSection extends React.Component{
     render(){
@@ -8,6 +9,7 @@ export default class FourStepsSection extends React.Component{
             <div className={styles.wrapper}>
                 <Decoration text={'Wystarczą cztery proste kroki'}/>
                 <div className={styles.__stepsBox}></div>
+                <Button text={'Załóż konto'} isSmall={false}/>
             </div>
         )
     }
