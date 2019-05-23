0cd4801dac1444e231b1ccf6ebf46c2bad646ee5
olawwska <olwojciech@gmail.com>
2019-05-23 21:03:55 +0200

    About Us section

--- src/components/AboutUsSection/AboutUsSection.jsx
+++ src/components/AboutUsSection/AboutUsSection.jsx
@@ -5,10 +5,12 @@ import Decoration from '../Decoration/Decoration';
 export default class AboutUsSection extends React.Component{
     render(){
         return(
-            <div className={styles.wrapper}>
-                <div><Decoration text={'O nas'}/>
-                    <p> Nori grape silver beet broccoli kombu beet greens fava bean potato quadong celery. Bunya nuts black-eyes pea prairie turnip leek lentil turnip greens parsnip.</p></div>
-                <div className={styles.__img}></div>
+            <div className={styles._wrapper}>
+                <div className={styles._sidebar}>
+                    <div><Decoration text={'O nas'}/>
+                        <p className={styles._paragraph}> Nori grape silver beet broccoli kombu beet greens fava bean potato quadong celery. Bunya nuts black-eyes pea prairie turnip leek lentil turnip greens parsnip.</p></div>
+                </div>
+                <div className={styles._img}></div>
             </div>
         )
     }
